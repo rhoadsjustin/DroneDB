7d319fbc3eefe32f21e80b5ad6f908a8c1d8f80a
Justin Rhoads <rhoadsjustin@gmail.com>
2017-06-20 13:06:40 -0500

    fixed state and prop routing to populate data from our database on the parts card

--- src/PartComp.js
+++ src/PartComp.js
@@ -4,9 +4,9 @@ import {CollapsibleItem} from 'react-materialize';
 class PartComp extends Component {
   render() {
     return(
-      <CollapsibleItem header={this.props.frame.name} icon='build'>
-        <p>{this.props.frame.price}</p>
-        <a href={this.props.frame.link}>Go to Part</a>
+      <CollapsibleItem header={this.props.name} icon='build'>
+        <p>${this.props.price}</p>
+        <a href={this.props.link}>Go to Part</a>
       </CollapsibleItem>
     )
   }
