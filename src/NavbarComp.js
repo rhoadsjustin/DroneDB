d07ba4366b66b5cdc2c75c619ac80361fff9a802
Benjamin Tovar <benjamintovar@Benjamins-MacBook-Pro.local>
2017-06-22 15:39:59 -0500

    Added the google auth functionality

--- src/NavbarComp.js
+++ src/NavbarComp.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import {Row, Navbar, NavItem} from 'react-materialize';
 import {Link} from 'react-router-dom';
-
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
 
 
 
 
 
 class NavbarComp extends Component {
+
+
+
 render() {
   return (
       <div>
@@ -15,9 +19,18 @@ render() {
           <Navbar className='navbar' brand='logo' right><Link to={'/'}></Link>
             <NavItem><Link to={'/build'}>Build a Drone</Link></NavItem>
             <NavItem><Link to={'/gallery'}>Gallery</Link></NavItem>
-            <NavItem><Link to={'/login'}>Login</Link></NavItem>
+            <NavItem>{ !this.props.currentUser ?
+              <NavItem><Link to='#' onClick={event => this.props.loginButtonClicked(event)}>Login</Link></NavItem> :
+              <NavItem><Link to='#' onClick={event => this.props.logoutButtonClicked(event)}>Logout</Link></NavItem>}
+            </NavItem>
+            <NavItem>
+              {this.props.currentUser != null ?
+                <h5>What up {this.props.currentUser.displayName}! </h5>
+              : <h5>What up dude or dudette!</h5>}
+            </NavItem>
           </Navbar>
         </Row>
+
       </div>
     )
   }
