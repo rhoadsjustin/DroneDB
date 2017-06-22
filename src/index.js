c83dc5fa0088070d11d2af6757f3e85cb122ebfc
Justin Rhoads <rhoadsjustin@gmail.com>
2017-06-22 16:53:29 -0500

    key config

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import dotenv from 'dotenv';
 import './index.css';
 
+dotenv.config({path: './.env'})
 ReactDOM.render(
   <App
   url='http://localhost:3001/api/parts' />,
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase';
 
 // TODO: replace with your project's customized code snippet
 const config = {
-  apiKey: "AIzaSyAgGI4-SAg79IDae_CvmeyUjp6NZ_cHCP4",
+    apiKey: "AIzaSyAgGI4-SAg79IDae_CvmeyUjp6NZ_cHCP4",
     authDomain: "dronedb-5d3c5.firebaseapp.com",
     databaseURL: "https://dronedb-5d3c5.firebaseio.com",
     projectId: "dronedb-5d3c5",
