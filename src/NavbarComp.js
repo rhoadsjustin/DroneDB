68905ee363e6697687480b18e877816683a55e0e
Justin Rhoads <rhoadsjustin@gmail.com>
2017-06-23 10:49:30 -0500

    removed whatup stranger

--- src/NavbarComp.js
+++ src/NavbarComp.js
@@ -22,11 +22,6 @@ render() {
               <NavItem><Link to='#' onClick={event => this.props.loginButtonClicked(event)}>Login</Link></NavItem> :
               <NavItem><Link to='#' onClick={event => this.props.logoutButtonClicked(event)}>Logout</Link></NavItem>}
             </NavItem>
-            <NavItem>
-              {this.props.currentUser != null ?
-                <h5>What up {this.props.currentUser.displayName}! </h5>
-              : <h5>What up dude or dudette!</h5>}
-            </NavItem>
           </Navbar>
         </Row>
 
