81a32f4c6f134373f2342dbe8d4dbc30b3f5768c
Justin Rhoads <rhoadsjustin@gmail.com>
2017-06-23 10:59:41 -0500

    welp

--- src/Gallery.js
+++ src/Gallery.js
@@ -3,35 +3,33 @@ import {Row} from 'react-materialize';
 import GalleryCard from './GalleryCard.js'
 
 class Gallery extends Component {
-  componentDidMount(){
+  render() {
+    console.log(this.props.allDrones);
     let allDronesNodes = this.props.allDrones.map(drone => {
       return(
-      <GalleryCard
-        key={drone['_id']}
-        droneFrame={drone['frame'].name}
-        droneFrameLink={drone['frame'].link}
-        droneESC={drone['electronicSpeedController'].name}
-        droneESCLink={drone['electronicSpeedController'].link}
-        droneMotor={drone['motor'].name}
-        droneMotorLink={drone['motor'].link}
-        droneFC={drone['flightController'].name}
-        droneFCLink={drone['flightController'].link}
-        droneProp={drone['propeller'].name}
-        dronePropLink={drone['propeller'].link}
-        droneVT={drone['videoTransmitter'].name}
-        droneVTLink={drone['videoTransmitter'].link}
-        droneCamera={drone['camera'].name}
-        droneCameraLink={drone['camera'].link}
-        droneGoggles={drone['goggle'].name}
-        droneGogglesLink={drone['goggle'].link}
-        droneTransmitter={drone['transmitter'].name}
-        droneTransmitterLink={drone['transmitter'].link}>
+        <GalleryCard
+          key={drone['_id']}
+          droneFrame={drone['frame'].name}
+          droneFrameLink={drone['frame'].link}
+          droneESC={drone['electronicSpeedController'].name}
+          droneESCLink={drone['electronicSpeedController'].link}
+          droneMotor={drone['motor'].name}
+          droneMotorLink={drone['motor'].link}
+          droneFC={drone['flightController'].name}
+          droneFCLink={drone['flightController'].link}
+          droneProp={drone['propeller'].name}
+          dronePropLink={drone['propeller'].link}
+          droneVT={drone['videoTransmitter'].name}
+          droneVTLink={drone['videoTransmitter'].link}
+          droneCamera={drone['camera'].name}
+          droneCameraLink={drone['camera'].link}
+          droneGoggles={drone['goggle'].name}
+          droneGogglesLink={drone['goggle'].link}
+          droneTransmitter={drone['transmitter'].name}
+          droneTransmitterLink={drone['transmitter'].link}>
         </GalleryCard>
       )
     })
-  }
-  render() {
-    console.log(this.props.allDrones);
     return (
       <div>
         <Row>
